1d93792d22e3d610d4b72b40d897e303b0eb01a1
Mayank Saharkar <95274436+Mayanksaharkar@users.noreply.github.com>
2025-07-06 13:58:58 +0000

    fix : unused imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Layout from './components/Layout/Layout';
 import HomePage from './components/pages/LandingPage/HomePage';
 import NotFound from './components/pages/NotFound.jsx';
-import Navbar from './components/Layout/Navbar';
-import PetSuggest from './components/pages/PetSuggest';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
 
 function App() {
