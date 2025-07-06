73d08a20349c12886e39ab1a4c8af5ad465fc6c9
Mayank Saharkar <mayanksaharkar@gmail.com>
2025-07-06 13:45:42 +0530

    add : homePage setup ; HeroSection

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 
 function Layout() {
   return (
      <div>
-      
+      <Navbar />
       <main>
         <Outlet />
       </main>
+      <Footer/>
      </div>
   );
 }
